ceea189e8011e80c15a2f5ddfc72b2cb148d23da
siquemarco <marco.gsfilho@gmail.com>
2020-02-12 22:48:37 -0300

    Challenge 29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function(DOM) {
   'use strict';
 
   /*
@@ -36,4 +36,71 @@
   que será nomeado de "app".
   */
 
-})();
+  function app() {
+    return {
+      init: function init() {
+        console.log('APP / INIT');
+        this.companyInfo();
+        this.initEvents();
+      },
+      initEvents: function initEvents() {
+        DOM('[data-js="form-register"]').on('submit', this.handleSubmit);
+      },
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
+        console.log('submit');
+        var $tableCar = DOM('[data-js="table-car"]').get();
+        $tableCar.appendChild(app().createNewCar());
+      },
+      createNewCar: function createNewCar() {
+        var $fragment = document.createDocumentFragment();
+        var $tr = document.createElement('tr');
+        var $tdImagem = document.createElement('td');
+        var $imagem = document.createElement('img');
+        var $tdBrand = document.createElement('td');
+        var $tdYear = document.createElement('td');
+        var $tdPlate = document.createElement('td');
+        var $tdColor = document.createElement('td');
+
+        $imagem.setAttribute('src', DOM('[data-js="image"]').get().value);
+        $tdImagem.appendChild($imagem);
+
+        $tdBrand.textContent = DOM('[data-js="brand-model"]').get().value;
+        $tdYear.textContent = DOM('[data-js="year"]').get().value;
+        $tdPlate.textContent = DOM('[data-js="plate"]').get().value;
+        $tdColor.textContent = DOM('[data-js="color"]').get().value;
+
+        $tr.appendChild($tdImagem);
+        $tr.appendChild($tdBrand);
+        $tr.appendChild($tdYear);
+        $tr.appendChild($tdPlate);
+        $tr.appendChild($tdColor);
+
+        return $fragment.appendChild($tr);
+      },
+      companyInfo: function companyInfo() {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', '/company.json', true);
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getCompanyInfo, false)
+      },
+      getCompanyInfo: function getCompanyInfo() {
+        if(!app().isReady.call(this)) {
+          return;
+        }
+        var data = JSON.parse(this.responseText);
+        var $companyName = DOM('[data-js="company-name"]').get();
+        var $companyPhone = DOM('[data-js="company-phone"]').get();
+        $companyName.textContent = data.name;
+        $companyPhone.textContent = data.phone;
+
+      },
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      }
+    }
+  }
+
+  app().init();
+
+})(window.DOM);
